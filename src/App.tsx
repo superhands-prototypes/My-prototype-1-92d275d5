6c45e5cab4c67a45a9cadcd2f7fe8c7f43c49352
grant <grant@superhands.ai>
2026-01-20 17:20:19 +0000

    Update from Superhands

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <div className="app">
       <div className="hello-container">
-        {['H', 'E', 'L', 'L', 'O'].map((letter, index) => (
+        {['H', 'E', 'Y', ' ', 'M', 'A', 'T', 'E'].map((letter, index) => (
           <span
             key={index}
             className="hello-letter"
